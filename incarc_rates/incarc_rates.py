2d936459332002fc237ec50c9c63158c3a0c28f9
Alyssa Crawford <alyssajcrawford1@email.arizona.edu>
2022-06-10 17:27:13 -0700

    Read data into dataframes

--- incarc_rates/incarc_rates.py
+++ incarc_rates/incarc_rates.py
@@ -7,3 +7,18 @@ Alyssa Crawford
 
 import pandas as pd, seaborn as sns, os, matplotlib.pyplot as plt
 
+
+# -- read all data into dataframes
+# men's incarceration data
+incarc_data_men = pd.read_excel('data/race_ethnicity_gender_2010.xlsx',
+        sheet_name = "Men",
+        skiprows = 4)
+# women's incarceration data
+incarc_data_women = pd.read_excel('data/race_ethnicity_gender_2010.xlsx',
+        sheet_name = "Women",
+        skiprows = 4)
+# all gender incarceration data
+incarc_data_total = pd.read_excel('data/race_ethnicity_gender_2010.xlsx',
+        sheet_name = "Total",
+        skiprows = 4)
+

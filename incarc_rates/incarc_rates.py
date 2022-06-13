8b412acbcfafa1379f45d63c541e74603187275e
Alyssa Crawford <alyssajcrawford1@email.arizona.edu>
2022-06-12 18:35:18 -0700

    Isolate desired columns

--- incarc_rates/incarc_rates.py
+++ incarc_rates/incarc_rates.py
@@ -18,3 +18,19 @@ incarc_data_women = pd.read_excel('data/race_ethnicity_gender_2010.xlsx',
 incarc_data_total = pd.read_excel('data/race_ethnicity_gender_2010.xlsx',
         sheet_name = "Total",
         skiprows = 4)
+
+# isolate only the total values, remove race/ethnicity data
+incarc_rates_men = incarc_data_men['Geography', 
+        'Total Men : In Correctional Facilities for Adults',
+        'Total Population: Male', 
+        'Male incarceration rate']
+incarc_rates_women = incarc_data_women['Geography', 
+        'Total Women : In Correctional Facilities for Adults',
+        'Total Population: Female', 
+        'Female incarceration rate']
+incarc_rates_total = incarc_data_men['Geography', 
+        'Total : In Correctional Facilities for Adults',
+        'Total Population', 
+        'Incarceration rate']
+
+print(incarc_rates_men)

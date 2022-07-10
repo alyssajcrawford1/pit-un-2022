7ef2a661d84445439542a079239ec8c17e69c142
alyssajcrawford1 <alyssajcrawford1@email.arizona.edu>
2022-07-10 14:15:57 -0700

    Random forest tutorial

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import os
+from sklearn.ensemble import RandomForestClassifier
 
 # Kaggle Titanic datasets
 train_data = pd.read_csv("titanic/data/train.csv")  # contains a "Survived" attribute
@@ -19,3 +20,17 @@ print("% of men who survived:", rate_men)
 
 
 
+# TEST: RANDOM FOREST
+y = train_data["Survived"]
+
+features = ["Pclass", "Sex", "SibSp", "Parch"]
+X = pd.get_dummies(train_data[features])
+X_test = pd.get_dummies(test_data[features])
+
+model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
+model.fit(X, y)
+predictions = model.predict(X_test)
+
+output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
+output.to_csv('titanic/output/test_submission.csv', index=False)
+print("Your submission was successfully saved!")

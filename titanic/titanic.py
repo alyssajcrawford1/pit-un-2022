7783e497f43a0dffb8e528e9af04b0b2dec73ae7
alyssajcrawford1 <alyssajcrawford1@email.arizona.edu>
2022-07-10 17:55:09 -0700

    Cleaned up

--- titanic/titanic.py
+++ titanic/titanic.py
@@ -8,29 +8,22 @@ train_data = pd.read_csv("titanic/data/train.csv")  # contains a "Survived" attr
 test_data = pd.read_csv("titanic/data/test.csv")  # no "Survived" attribute
 
 
-
-# TEST THEORY: Women survive and men die
-women = train_data.loc[train_data.Sex == 'female']["Survived"]
-rate_women = sum(women)/len(women)
-print("% of women who survived:", rate_women)
-
-men = train_data.loc[train_data.Sex == 'male']["Survived"]
-rate_men = sum(men)/len(men)
-print("% of men who survived:", rate_men)
-
-
-
-# TEST: RANDOM FOREST
+# RANDOM FOREST
 y = train_data["Survived"]
 
 features = ["Pclass", "Sex", "SibSp", "Parch"]
 X = pd.get_dummies(train_data[features])
-X_test = pd.get_dummies(test_data[features])
 
+# train using data, build model
 model = RandomForestClassifier(n_estimators=100, max_depth=5, random_state=1)
 model.fit(X, y)
+
+# use model to predict test data survival
+X_test = pd.get_dummies(test_data[features])
 predictions = model.predict(X_test)
 
+
+
+# save model results as csv
 output = pd.DataFrame({'PassengerId': test_data.PassengerId, 'Survived': predictions})
-output.to_csv('titanic/output/test_submission.csv', index=False)
-print("Your submission was successfully saved!")
+output.to_csv('titanic/output/submission1.csv', index=False)
